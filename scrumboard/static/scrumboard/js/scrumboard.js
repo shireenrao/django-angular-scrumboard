3bd9d1009f2d1bd37d7d6f2110f70c7a050f4af9
Srinivas Nyayapati <shireenrao@gmail.com>
2017-06-10 18:38:19 -0400

    Add post of data from angular page to drf

--- scrumboard/static/scrumboard/js/scrumboard.js
+++ scrumboard/static/scrumboard/js/scrumboard.js
@@ -7,14 +7,22 @@
     function ScrumboardController($scope, $http) {
         $scope.add = function(list, title) {
             var card = {
+                list: list.id,
                 title: title
             };
-            list.cards.push(card);
+            $http.post('/scrumboard/cards/', card)
+                .then(function(response){
+                    list.cards.push(response.data);
+                },
+                function(){
+                    alert('could not create card!');
+                });
+
         };
 
         $scope.data = [];
 
-        $http.get('/scrumboard/lists').then(function(response) {
+        $http.get('/scrumboard/lists/').then(function(response) {
             $scope.data = response.data;
         });
 
